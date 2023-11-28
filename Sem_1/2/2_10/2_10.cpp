7703e1b648e4acbcaaf83a02704ba1adaaf70233
MOkASiH <146427018+MOkASiH@users.noreply.github.com>
2023-11-28 15:22:37 +0300

    Add files via upload

---
+++ Sem_1/2/2_10/2_10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int n, s, max;
+	cout << "введиите кол-во символов: ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> s;
+		if (i == 1) {
+			for (int j = 1; j < 2; j++) {
+				max = s;
+			}
+		}
+		if (s >= max) {
+			max = s;
+		}
+	}
+	cout << max;
+
+	return 0;
+}
