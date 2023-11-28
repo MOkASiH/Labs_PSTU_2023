390e87b0a9d4b5105bf5872f219565cb8c9838e6
MOkASiH <146427018+MOkASiH@users.noreply.github.com>
2023-11-28 19:31:46 +0300

    Add files via upload

---
+++ Sem_1/2/2_16/2_16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int n, sum = 0;
+
+	cout << "введите кол-во слагаемых: ";
+	cin >> n;
+
+
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 != 0) {
+			sum -= i;
+		}
+		else{
+			sum += i;
+		}
+	}
+	cout << sum;
+
+	return 0;
+}
