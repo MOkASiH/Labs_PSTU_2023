c1447452dc24c84ced7343a8ed4a2821da3fc315
MOkASiH <146427018+MOkASiH@users.noreply.github.com>
2023-12-04 18:48:44 +0300

    Add files via upload

---
+++ Sem_1/2/2_23/2_23.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int s = 0;
+	float max, a, n;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+
+		a = sin(n + i / n);
+
+		if (i == 1) {
+			max = a;
+		}
+		if (a > max) {
+			max = a;
+			s = 0;
+		}
+		if (max == a) {
+			s += 1;
+		}
+
+		cout << a << ", " << endl;
+	}
+	cout << max << "; " << s;
+
+	return 0;
+}
