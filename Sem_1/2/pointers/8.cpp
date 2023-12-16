9403c2abdafa56d74e5c112aa07f6a7e4ec05150
MOkASiH <146427018+MOkASiH@users.noreply.github.com>
2023-12-16 20:35:07 +0300

    Add files via upload

---
+++ Sem_1/2/pointers/8.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string s, a, b;
+
+	cin >> s >> a >> b;
+
+	int lc = 0;
+	int uc = 0;
+	int other = 0;
+	int l = s.length();
+
+	for (int i = 0; i < l; i++) {
+
+		string sim = s.substr(i, 1);
+
+		if (sim == a)
+			uc++;
+		else if (sim == b)
+			lc++;
+		else
+			other++;
+	}
+
+	cout << uc << endl << lc << endl << other << endl << l;
+
+	return 0;
+}
