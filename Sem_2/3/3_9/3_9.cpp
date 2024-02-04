a24cc5e7a37e5c826b6cc27b444a392fb35b169b
MOkASiH <146427018+MOkASiH@users.noreply.github.com>
2024-02-04 15:38:05 +0300

    Add files via upload

---
+++ Sem_2/3/3_9/3_9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int n;
+    int arr[100];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            for (int j = i; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            i--;
+            n--;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
