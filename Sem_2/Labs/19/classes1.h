6464879507786b907ef8675ccee77844bf089045
MOkASiH <146427018+MOkASiH@users.noreply.github.com>
2024-04-07 20:18:44 +0500

    Update classes1.h

--- Sem_2/Labs/19/classes1.h
+++ Sem_2/Labs/19/classes1.h
@@ -2,16 +2,10 @@
 
 class Progression {
 public:
-
     double first;
     int second;
-    int j;
-
-    double element() {
-        double j_elem;
-        for (int i = 1; i <= j; i++) {
-            j_elem = first * pow(second, j);
-        }
-        return j_elem;
-    }
+    void init(double, int);
+    void show();
+    void read();
+    double element(int);
 };
