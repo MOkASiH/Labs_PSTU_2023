f6d64e2d9ed96216f0fb79dfff46bb6c019dee12
MOkASiH <146427018+MOkASiH@users.noreply.github.com>
2024-04-30 00:59:42 +0500

    ...

---
+++ Sem_2/Labs/Classes/13/task_3/Map_function.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include <map>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <algorithm>
+
+
+using namespace std;
+
+void  Push_average_value(map <int, Pair>& m_map) {
+	if (m_map.size() > 0) {
+		int MaxKey = max_element(m_map.begin(), m_map.end(), [](const auto& p1, const auto& p2) {
+			return p1.first < p2.first;
+			})->first + 1;
+		Pair Middle_second = accumulate(m_map.begin(), m_map.end(), Pair(0,0),[](Pair value, const auto& p) {
+			return value + p.second;
+			}) / m_map.size();
+		m_map[MaxKey] = Middle_second;
+		m_map.insert({ MaxKey, Middle_second });
+	}
+	else {
+		cout << "Словарь пуст!\n";
+	}
+}
+
+void Delete_range_keys(map <int, Pair>& m_map, const double BeginValue, const double EndValue) {
+	for (auto p = m_map.begin(); p != m_map.end();) {
+		if (BeginValue <= (*p).second && (*p).second <= EndValue) {
+			p = m_map.erase(p);
+		}
+		else {
+			p++;
+		}
+	};
+}
+
+void Plus_sum_Min_Max(map <int, Pair>& m_map) {
+	if (m_map.size() > 0) {
+		Pair MaxValue = max_element(m_map.begin(), m_map.end(), [](const auto& p1, const auto& p2) {
+			return p1.second < p2.second;
+			})->second;
+		Pair MinValue = min_element(m_map.begin(), m_map.end(), [](const auto& p1, const auto& p2) {
+			return p1.second < p2.second;
+			})->second;
+		for (auto& p : m_map) {
+			p.second = p.second +MaxValue + MinValue;
+		}
+	}
+}
+
+void Show_map(const map <int, Pair>& m_map) {
+	if (m_map.size() > 0) {
+		for (auto& p : m_map) {
+			cout << p.first << ") " << p.second << '\n';
+		}
+	}
+	else {
+		cout << "Словарь пуст!";
+	}
+	cout << '\n';
+}
---
+++ Sem_2/Labs/Classes/13/task_3/infa.cpp
@@ -0,0 +1,29 @@
+﻿#include "Pair.h"
+#include "Map_function.h"
+
+
+int main() {
+    system("chcp 1251 > null");
+
+    map <int, Pair> a = { {1, {2, 3.14}}, {2, {4, 5.44}}, {3, {12, 2.15}}, {4, {2, 6.28}} };
+    cout << "Исходный словарь" << endl;
+    Show_map(a);
+
+    cout << "Словарь после добавления среднего значения в конец" << endl;
+    Push_average_value(a);
+    Show_map(a);
+
+    double begin, end;
+    cout << "Введите диапозон, элементы содержащие ключи, внутри которого будут удалены\n";
+    cin >> begin;
+    cin >> end;
+    Delete_range_keys(a, begin, end);
+    cout << "Словаь, после удаления элементов содержащих ключи в заданном диапазоне\n";
+    Show_map(a);
+
+    cout << "Словарь, после добавления к каждому элементу суммы максимального и минимального элементов\n";
+    Plus_sum_Min_Max(a);
+    Show_map(a);
+
+    return 0;
+}
